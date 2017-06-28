3242a05693f6bd5c993b22c59b386be0115f09c1
bondarum <marcin.bondaruk@gmail.com>
2017-06-28 14:41:18 +0200

    ConvertKit: Client Backend

--- src/Response.php
+++ src/Response.php
@@ -19,6 +19,26 @@
 			return isset( $this->data[ 'result' ] ) ? $this->data[ 'result' ] : null;
 		}
 
+		public function getError()
+		{
+			if ( isset( $this->data[ 'errors' ] ) && count( $this->data[ 'errors' ] ) )
+			{
+				return $this->data[ 'errors' ][ 0 ];
+			}
+
+			return null;
+		}
+
+		public function getWarning()
+		{
+			if( isset( $this->data[ 'warnings' ] ) && count( $this->data[ 'warnings' ] ) )
+			{
+				return $this->data[ 'warnings' ][ 0 ];
+			}
+
+			return null;
+		}
+
 		public static function fromCommand( OperationCommand $command )
 		{
 			return new static( $command->getResponse()->json() );
